4ec69b032f3c9afe709761ac9266977e689cc45a
licetus <licetus0316@gmail.com>
2019-01-13 01:18:40 +0800

    split center content to show schema & swagger

---
+++ src/components/content-schema.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>schema-content</div>
+</template>
+
+<script>
+import api from '../facades/api'
+// import { Schema, Column } from '../libs/schema'
+
+export default {
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+    },
+  },
+  methods: {
+    async getSchema() {
+      try {
+        const res = await api.schema.get(this.index)
+        this.schema = res
+      } catch (error) {
+        this.$message.error(error.message)
+      }
+    },
+  },
+  mounted() {
+    this.getSchema()
+  },
+}
+</script>
--- src/views/manager.vue
+++ src/views/manager.vue
@@ -4,37 +4,8 @@
       <Explorer @change="handleChangeSchema" />
     </div>
     <div class="content-panel">
-      <template v-for="(tag, tindex) of newSwagger.tags">
-        <div :key="tindex">
-          <a-divider orientation="left">
-            {{ tag.name }}
-          </a-divider>
-          <a-collapse :accordion="isAccordion">
-            <template v-for="(pathName, pindex) of Object.keys(newSwagger.paths)">
-              <template v-for="(funcName, findex) of funcMap">
-                <a-collapse-panel
-                  :key="`${pindex}_${findex}`"
-                  v-if="newSwagger.paths[pathName][funcName] && newSwagger.paths[pathName][funcName].tags.includes(tag.name)"
-                >
-                  <div slot="header">
-                    <span>{{ funcName.toUpperCase() }}: </span>
-                    <span>{{ pathName }}</span>
-                  </div>
-                  <a-collapse :accordion="isAccordion">
-                    <a-collapse-panel>
-                      <div slot="header">
-                        <span>operationId: </span>
-                        <span>{{ newSwagger.paths[pathName][funcName].operationId }}</span>
-                      </div>
-                      <p>text......</p>
-                    </a-collapse-panel>
-                  </a-collapse>
-                </a-collapse-panel>
-              </template>
-            </template>
-          </a-collapse>
-        </div>
-      </template>
+      <ContentSwagger v-if="isSwagger" />
+      <ContentSchema v-if="isSchema" />
     </div>
     <div class="right-panel">
       <CodeView :schema="currentSchema" />
@@ -43,31 +14,22 @@
 </template>
 
 <script>
-import { cloneDeep } from 'lodash'
 import Explorer from '../components/explorer.vue'
 import CodeView from '../components/code-view.vue'
-import swagger from '../../swagger/swagger'
-// import { Task } from '../../../ymir-models/src/models'
+import ContentSwagger from '../components/content-swagger.vue'
+import ContentSchema from '../components/content-schema.vue'
 
-const funcMap = ['get', 'post', 'patch', 'delete']
-// const schemas = [
-//   new Task(),
-//   { schemaName: 'user' },
-//   { schemaName: 'profile' },
-//   { schemaName: 'question' },
-//   { schemaName: 'answer' },
-// ]
 export default {
   components: {
     Explorer,
     CodeView,
+    ContentSwagger,
+    ContentSchema,
   },
   data() {
     return {
-      newSwagger: cloneDeep(swagger),
-      funcMap,
-
-      isAccordion: false,
+      isSwagger: false,
+      isSchema: true,
       currentSchema: null,
     }
   },
@@ -81,12 +43,8 @@ export default {
     handleChangeSchema(schema) {
       this.currentSchema = schema
     },
-    initPage() {
-      console.log(swagger)
-    },
   },
   mounted() {
-    this.initPage()
   },
 }
 </script>
