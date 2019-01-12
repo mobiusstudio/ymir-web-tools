a79a0eddb5d348c0b29364a7004e2146dde7f200
licetus <licetus0316@gmail.com>
2019-01-13 01:01:09 +0800

    add schema api

--- src/facades/api.js
+++ src/facades/api.js
@@ -1,12 +1,41 @@
-export const api = {}
+const api = {}
 
 api.schema = {
-  get: async () => {
+  save: (schemaArray) => {
+    const buffer = JSON.stringify(schemaArray)
+    localStorage.setItem('schemas-data', buffer)
   },
-  add: async () => {
+  list: async () => {
+    const schemaArray = JSON.parse(localStorage.getItem('schemas-data')) || []
+    return schemaArray
   },
-  update: async () => {
+  get: async (index) => {
+    const schemaArray = JSON.parse(localStorage.getItem('schemas-data')) || []
+    return schemaArray[index]
   },
-  delete: async () => {
+  add: async (schema) => {
+    const schemaArray = JSON.parse(localStorage.getItem('schemas-data')) || []
+    if (schemaArray.every(item => item.schemaName !== schema.schemaName)) {
+      schemaArray.push(this.currentSchema)
+      this.save(schemaArray)
+    } else {
+      throw new Error(`Duplicate schema name ${schema.schemaName}`)
+    }
+  },
+  update: async (schema, index) => {
+    const schemaArray = JSON.parse(localStorage.getItem('schemas-data')) || []
+    if (schemaArray.some((item, i) => item.schemaName === schema.schemaName && i !== index)) {
+      throw new Error(`Duplicate schema name ${schema.schemaName}`)
+    } else {
+      schemaArray[index] = schema
+      this.save(schemaArray)
+    }
+  },
+  delete: async (index) => {
+    const schemaArray = JSON.parse(localStorage.getItem('schemas-data')) || []
+    schemaArray.splice(index, 1)
+    this.save(schemaArray)
   },
 }
+
+export default api
