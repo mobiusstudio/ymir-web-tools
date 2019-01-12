b162ce7f8983d631301b9800e2a7a5c7ae747ea1
licetus <licetus0316@gmail.com>
2019-01-13 01:01:26 +0800

    add schema libs

---
+++ src/libs/schema.js
@@ -0,0 +1,34 @@
+import { BaseColumn } from '../../../ymir-models/core/model/column-base'
+
+export class Column extends BaseColumn {
+  constructor({
+    table = '',
+    type,
+    name,
+    alias = null,
+    foreign = null,
+    def = null,
+    required = false,
+  }) {
+    super({ table, type, name, alias, foreign, def, required })
+  }
+
+  static propMap = ['type', 'name', 'alias', 'foreign']
+}
+
+export class Schema {
+  constructor({
+    schemaName,
+    tableName,
+    pkeyIndex = 0,
+    columns = new Column({
+      type: 'id',
+      name: 'id',
+    }),
+  }) {
+    this.schemaName = schemaName
+    this.tableName = tableName
+    this.pkeyIndex = pkeyIndex
+    this.columns = columns
+  }
+}
